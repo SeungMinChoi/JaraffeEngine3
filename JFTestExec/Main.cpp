06bd60a0743d2bc621d99104c408686a8ea1daff
SeungMinChoi <ccm1333@naver.com>
2019-05-26 23:58:29 +0900

    Test코드 추가

--- JFTestExec/Main.cpp
+++ JFTestExec/Main.cpp
@@ -1,5 +1,6 @@
 //#include "JFEngine.h"
 #include "Foundation/JFFunction.h"
+#include "Foundation/JFTypeTraits.h"
 
 #include <iostream>
 //#include <Windows.h>
@@ -17,17 +18,77 @@ void Test()
 
 }
 
+template<class T>
+struct TestFunction
+{
+};
+
+template<class R, class... P>
+struct TestFunction<R(P...)>
+{
+	using Type = R(*)(P...);
+
+	TestFunction()
+	{
+
+	}
+	TestFunction(Type t)
+		: testMember(t)
+	{}
+
+	TestFunction& operator = (const TestFunction t)
+	{
+		testMember = t.testMember;
+		return *this;
+	}
+
+	R Invoke(P... params)
+	{
+		return testMember(params...);
+	}
+
+	Type testMember;
+};
+
 int main()
 {
 	auto lambda = []()
 	{};
-
 
 	std::function<void()> qwe;
 
 	JFFunction FuncTest1 = [](){};
 	JFFunction FuncTest2 = lambda;
 	JFFunction FuncTest3(Test);
+
+	TestFunction<int()> TestFunction1;
+	{
+		int a = 5;
+		int b = 3;
+
+		auto lambda2 = [&a, &b]()
+		{
+			return a + b;
+		};
+
+		auto lambda3 = []()
+		{
+			return 0;
+		};
+
+		//TestFunction<int()> z(lambda2);
+		//TestFunction<int()> z2(lambda3);
+
+		using LambdaType2 = decltype(lambda2);
+		using LambdaType3 = decltype(lambda3);
+
+		//TestFunction1 = TestFunction<int()>(lambda2);
+		auto t = std::is_trivially_copyable<decltype(&LambdaType2::operator())>::value;
+		auto t2 = std::is_trivially_copyable<decltype(&LambdaType3::operator())>::value;
+		int qwe = 0;
+	}
+
+	TestFunction1.Invoke();
 
     //JFSpinLock s;
     //JFScopeLock<JFSpinLock> sl(s);
